0f9c6b085092a2119866670094de919cdc97cd95
halwohamda <halwohamda@gmail.com>
2024-01-26 08:31:08 -0800

    done

--- Client/src/Components/Pages/Employee/PostEmployee.jsx
+++ Client/src/Components/Pages/Employee/PostEmployee.jsx
@@ -4,9 +4,9 @@ import { RiArrowGoBackFill } from "react-icons/ri";
 const PostEmployee = () => {
   return (
     <div>
-     <div className="container -translate-y-[300px]">
+     <div className="container -translate-y-[400px]">
         <h1 className='text-center text-4xl -translate-y-24 ml-44 font-bold uppercase text-gray-300 underline'>Register Employee</h1>
-      <form className='ml-[500px] -translate-y-[120px] shadow-md shadow-gray-200 p-4 w-[500px]'>
+      <form className='ml-[500px] -translate-y-[100px] shadow-md shadow-gray-200 p-4 w-[500px]'>
         <input type="text" placeholder='FirstName' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg ' /> 
         <input type="text" placeholder='MiddleName' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg ml-2' />
         <input type="text" placeholder='LastName' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg ' />
