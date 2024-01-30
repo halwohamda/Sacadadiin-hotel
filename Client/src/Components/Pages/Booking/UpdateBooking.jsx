f6a17b7c2ccba9633ceb2ca5596275fa93e19259
halwohamda <halwohamda@gmail.com>
2024-01-30 08:37:22 -0800

    done

--- Client/src/Components/Pages/Booking/UpdateBooking.jsx
+++ Client/src/Components/Pages/Booking/UpdateBooking.jsx
@@ -8,9 +8,9 @@ const UpdateBooking = () => {
         <h1 className='text-center text-4xl -translate-y-24 ml-44 font-bold uppercase text-orange-600 underline'>Update Booking</h1>
       <form className='ml-[500px] -translate-y-[100px] shadow-md shadow-gray-200 p-4 w-[500px]'>
         <input type="number" placeholder='booking_id' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg ' /> 
-        <input type="text" placeholder='RoomNumer' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg ml-2' />
-        <input type="text" placeholder='Guest_ID' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg mt-2' /> 
-        <input type="text" placeholder='Total_Price' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg mt-2 ml-2' /> <br />
+        <input type="number" placeholder='RoomNumer' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg ml-2' />
+        <input type="number" placeholder='Guest_ID' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg mt-2' /> 
+        <input type="numer" placeholder='Total_Price' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg mt-2 ml-2' /> <br />
         <input type="submit" className='pt-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg mt-2 w-[444px] bg-orange-600 text-2xl text-white border-0' /> <br />
       </form>
      <div className="box ml-[1200px] -mt-[440px] text-4xl">
