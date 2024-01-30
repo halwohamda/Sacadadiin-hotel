6ed626c29e09224934994efc9445a0aff1ced408
halwohamda <halwohamda@gmail.com>
2024-01-30 08:40:10 -0800

    done

--- Client/src/Components/Pages/Booking/PostBooking.jsx
+++ Client/src/Components/Pages/Booking/PostBooking.jsx
@@ -5,7 +5,7 @@ const PostBooking = () => {
   return (
     <div>
      <div className="container -translate-y-[400px]">
-        <h1 className='text-center text-4xl -translate-y-24 ml-44 font-bold uppercase text-gray-300 underline'>Register Employee</h1>
+        <h1 className='text-center text-4xl -translate-y-24 ml-44 font-bold uppercase text-gray-300 underline'>Register Booking</h1>
       <form className='ml-[500px] -translate-y-[100px] shadow-md shadow-gray-200 p-4 w-[500px]'>
         <input type="text" placeholder='FirstName' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg ' /> 
         <input type="text" placeholder='MiddleName' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg ml-2' />
