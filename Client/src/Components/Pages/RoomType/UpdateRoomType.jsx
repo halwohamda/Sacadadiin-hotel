7ec61bdbb1e9488a3b10af4b3f85f5d90ab10937
halwohamda <halwohamda@gmail.com>
2024-01-30 08:59:20 -0800

    done

--- Client/src/Components/Pages/RoomType/UpdateRoomType.jsx
+++ Client/src/Components/Pages/RoomType/UpdateRoomType.jsx
@@ -8,9 +8,9 @@ const UpdateRoomType = () => {
         <h1 className='text-center text-4xl -translate-y-24 ml-44 font-bold uppercase text-orange-600 underline'>Update RoomType</h1>
       <form className='ml-[500px] -translate-y-[100px] shadow-md shadow-gray-200 p-4 w-[500px]'>
         <input type="number" placeholder='RoomType_id' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg ' /> 
-        <input type="number" placeholder='Name' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg ml-2' />
+        <input type="text" placeholder='Name' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg ml-2' />
         <input type="number" placeholder='PriceperNight' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg mt-2' /> 
-        <input type="numer" placeholder='Description' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg mt-2 ml-2' /> <br />
+        <input type="text" placeholder='Description' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg mt-2 ml-2' /> <br />
         <input type="numer" placeholder='capacity' className='pt-2 border-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg mt-2 ml-2' /> <br />
         <input type="submit" className='pt-2 px-4 justify-center m-auto translate-x-2 p-2 rounded-lg mt-2 w-[444px] bg-orange-600 text-2xl text-white border-0' /> <br />
       </form>
